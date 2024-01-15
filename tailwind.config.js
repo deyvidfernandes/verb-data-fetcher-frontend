e06369f56f4bd23f931711bbd152dc4db5400983
deyvidfernandes <deyvid.guedes.fer@gmail.com>
2024-01-15 15:41:09 -0300

    feat: configure app colors on tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+    fontFamily: {
+      sans: ['"Inter", sans-serif']
+    },
+		colors: {
+      white: '#FFF',
+			green: '#6FD93D',
+			red: '#D93D3D',
+			gray: '#999999',
+			orange: '#FF9A24',
+		},
+	},
+	plugins: [],
 }
-

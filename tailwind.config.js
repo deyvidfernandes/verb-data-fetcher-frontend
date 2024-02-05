d184564529ace0850ef3825a0bce955d1ce8dbc2
deyvidfernandes <deyvid.guedes.fer@gmail.com>
2024-02-05 19:17:30 -0300

    feat: change theme config to include values from the default theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
-    fontFamily: {
-      sans: ['"Inter", sans-serif']
-    },
-		colors: {
-      white: '#FFF',
-			green: '#6FD93D',
-			red: '#D93D3D',
-			gray: '#999999',
-			'light-gray': '#DDD',
-			orange: '#FF9A24',
+		extend: {
+			fontFamily: {
+				sans: ['"Inter", sans-serif'],
+			},
+			colors: {
+				brandBlue: '#66A3FF',
+				brandGreen: '#6FD93D',
+				brandRed: '#D93D3D',
+				brandOrange: '#FF9A24',
+				brandScrollOrange: '#FF9A2499',
+				brandScrollOrangeHover: '#FF9A24CC',
+			},
 		},
 	},
 	plugins: [],

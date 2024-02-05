77944142876f3809c478b935d79c562c0dbb74f2
deyvidfernandes <deyvid.guedes.fer@gmail.com>
2024-02-05 20:37:20 -0300

    feat: add renderingOnceRef hook

---
+++ src/util/hooks/useRenderingOnceRef.ts
@@ -0,0 +1,11 @@
+import { useEffect, useRef, useState } from 'react'
+
+export const useRerenderingOnceRef = <I>() => {
+	const ref = useRef<I>()
+	const [, setRefAcquired] = useState(false)
+
+	useEffect(() => {
+		setRefAcquired(true)
+	}, [])
+	return ref
+}

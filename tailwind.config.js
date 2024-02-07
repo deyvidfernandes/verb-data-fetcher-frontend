9627e2750ee2c5658b07edcc757426c89219788e
deyvidfernandes <deyvid.guedes.fer@gmail.com>
2024-02-07 17:51:37 -0300

    feat: add new colors to the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,11 @@ export default {
 			colors: {
 				brandBlue: '#66A3FF',
 				brandGreen: '#6FD93D',
+				brandLightGreen: 'hsl(101, 67%, 65%)',
+				brandDarkGreen: '#57c026',
 				brandRed: '#D93D3D',
+				brandLightRed: 'hsl(0, 67%, 65%)',
+				brandDarkRed: 'rgb(192, 38, 38)',
 				brandOrange: '#FF9A24',
 				brandScrollOrange: '#FF9A2499',
 				brandScrollOrangeHover: '#FF9A24CC',

3d9becad1c7b63c5805a79382577ead1da15697c
deyvidfernandes <deyvid.guedes.fer@gmail.com>
2024-02-08 11:12:22 -0300

    feat: added variants to the TextDisplay component

--- src/components/basic/TextDisplay.tsx
+++ src/components/basic/TextDisplay.tsx
@@ -1,15 +1,20 @@
-import { ComponentPropsWithoutRef } from 'react'
+import { ComponentPropsWithoutRef, ReactNode } from 'react'
 
 interface Props extends ComponentPropsWithoutRef<'p'> {
 	label: string
-	value: string | number
+	value: string | number | ReactNode
+	subInfo?: boolean
+	bigger?: boolean
 }
 
 export const TextDisplay = (props: Props) => {
-	const { label, value, ...builtInProps } = props
+	const { label, value, subInfo, bigger, ...builtInProps } = props
 
 	return (
-		<p className='text-sm' {...builtInProps}>
+		<p
+			className={`${bigger ? 'text-base' : 'text-sm'} ${subInfo && 'text-gray-700'}`}
+			{...builtInProps}
+		>
 			<span className='font-medium'>{label}: </span>
 			{value}
 		</p>

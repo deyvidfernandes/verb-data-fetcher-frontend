072bc6200afdf000cc917f613fbedd6e41320b81
deyvidfernandes <deyvid.guedes.fer@gmail.com>
2024-02-14 13:45:22 -0300

    refactor: redesign useSetDatabaseConnection hook

--- src/api/database/useSetDatabaseConnection.ts
+++ src/api/database/useSetDatabaseConnection.ts
@@ -1,21 +1,15 @@
 import { ErrorResponse } from './metadata'
 import { BACKEND_API_ADDRESS } from './metadata'
-import { useGlobalStateContext } from '@/util/globalState/GlobalStateContext'
-import { INITIAL_GLOBAL_STATE } from '@/util/globalState/INITIAL_GLOBAL_STATE'
-import { ProcessStatus, RawVerb } from '@/util/globalState/types'
 import { useState } from 'react'
 import { DBConfigValues } from '../../content/forms/DBConfigValues'
 
 export const useSetDatabaseConnection = () => {
-	const dispatchGlobalAction = useGlobalStateContext((v) => v.dispatchGlobalAction)
 
-	const [dbRequestError, setDBRequestError] = useState<DBConfigRequestError>()
-	const [hasServerResponded, setHasServerResponded] = useState(false)
+	const [responseData, setResponseData] = useState<RequestResponse>()
 
-	const tryDatabaseConnection = async (
+	const request = async (
 		values: DBConfigValues,
-		changingConfig?: boolean,
-	) => {
+	): Promise<boolean> => {
 		const postDBConfig = async () => {
 			const { url, type, name } = values
 			const body = {
@@ -34,80 +28,42 @@ export const useSetDatabaseConnection = () => {
 		}
 
 		const handleResponse = async (res: Response) => {
-			const { baseDataFile, outputMethod } = values
-
-			const updateAppGlobalState = async () => {
-				if (changingConfig) {
-					dispatchGlobalAction({
-						type: 'CHANGE_DATABASE_CONFIGURATION',
-						payload: {
-							database: {
-								...values,
-							},
-							...outputMethod,
-						},
-					})
-					return
-				}
-
-				if (!baseDataFile) throw new Error()
-				const rawVerbData: RawVerb[] = JSON.parse(await baseDataFile?.text())
-				dispatchGlobalAction({
-					type: 'SETUP_PROCESS',
-					payload: {
-						database: {
-							...(outputMethod.persistData ? values : initialDBConfigValues),
-						},
-						dataSource: {
-							rawVerbData,
-							fileName: baseDataFile.name,
-							fileSize: baseDataFile.size
-						},
-						...outputMethod,
-					},
-				})
-
-				dispatchGlobalAction({
-					type: 'CHANGE_STATUS',
-					payload: {
-						status: ProcessStatus.FETCHING,
-					},
-				})
-			}
 
 			const handleErrorResponse = async (res: Response) => {
 				const errorData: ErrorResponse = await res.json()
-				setDBRequestError({
-					message: errorData.message,
-					exceptionMessage: errorData.exceptionMessage,
+				setResponseData({
+					error: {
+						message: errorData.message,
+						exceptionMessage: errorData.exceptionMessage,
+					},
 				})
 			}
 
-			setHasServerResponded(true)
+			setResponseData({})
 			if (!res.ok) {
 				handleErrorResponse(res)
-				return
+				return false
 			}
-			updateAppGlobalState()
+			return true
 		}
 
 		const configRequestResponse = await postDBConfig()
-		handleResponse(configRequestResponse)
+		return handleResponse(configRequestResponse)
 	}
 
-	const cleanConnectionAttempt = () => {
-		setDBRequestError(undefined)
-		setHasServerResponded(false)
+	const cleanAttempt = () => {
+		setResponseData(undefined)
 	}
-
-	const initialDBConfigValues = INITIAL_GLOBAL_STATE.processConfiguration.database
 
 	return {
-		tryDatabaseConnection,
-		cleanConnectionAttempt,
-		hasServerResponded,
-		dbRequestError,
+		request,
+		cleanAttempt,
+		responseData,
 	}
+}
+
+export interface RequestResponse {
+	error?: DBConfigRequestError
 }
 
 export interface DBConfigRequestError {

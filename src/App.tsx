d852957cd4a65fb22ec6268a29748c443b0b3242
deyvidfernandes <deyvid.guedes.fer@gmail.com>
2024-03-02 10:59:26 -0300

    fix: correct App rendering twice

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,51 @@
+import { PanelWithTabs } from '@/components/panel/PanelWithTabs'
+import { tabs } from './content/controlPanelTabs/tabs'
+import { GlobalStateProvider } from './util/globalState/GlobalStateContext'
+import { ControlButton } from './content/ControlButton'
+import { Section } from './components/basic/Section'
+import { VerbFetcher } from './content/verbFetcher/VerbFetcher'
+import { ControlledLocalStorage } from './util/globalState/ControlledLocalStorage'
+
 function App() {
-	return <></>
+	ControlledLocalStorage.init()
+
+	return (
+		<GlobalStateProvider>
+			<h1 className='mt-4 font-semibold text-5xl'>Verb Data Fetcher</h1>
+
+			<Section title='Control Panel'>
+				<PanelWithTabs tabs={tabs} />
+				<ControlButton/>
+			</Section>
+
+			<VerbFetcher />
+
+			<Section title='About'>
+				<p>
+					The Verb Data Fetcher is a tool designed to enrich data about irregular verbs in
+					English. Users input a JSON file containing verbs in infinitive, simple past,
+					and present perfect formats. The application utilizes the Free Dictionary API to
+					obtain detailed definitions and the Google NGram API to determine the usage
+					frequency of verbs.
+				</p>
+				<p>
+					After processing, the tool generates a new enriched JSON file, including
+					dictionary definitions, English pronunciations for all forms, and usage
+					frequencies. This enhanced file is then sent to the backend, where it undergoes
+					analysis and is stored persistently as records in the database.
+				</p>
+				<p>
+					This tool was developed on a non-profit basis and is open under the MIT license.
+					The generated data will be used to feed the Master of Tenses application, a
+					final project for the Technical Systems Development course at ETEC.
+				</p>
+			</Section>
+
+			<footer>
+				<p>Deyvid Fernandes - 2022</p>
+			</footer>
+		</GlobalStateProvider>
+	)
 }
 
 export default App
--- src/content/ControlButton.tsx
+++ src/content/ControlButton.tsx
@@ -2,15 +2,13 @@ import Button from '@/components/basic/Button'
 import { ModalInterface } from '@/components/basic/modal/Modal'
 import { useGlobalStateContext } from '@/util/globalState/GlobalStateContext'
 import { ProcessStatus } from '@/util/globalState/types'
-import { RefObject } from 'react'
 import lockIcon from '@/assets/lock-solid.svg'
+import { useRerenderingOnceRef } from '@/util/hooks/useRenderingOnceRef'
+import { SetupModal } from './modals/SetupModal'
 
-interface Props {
-	setupModalRef: RefObject<ModalInterface>
-}
-
-export const ControlButton = ({ setupModalRef }: Props) => {
+export const ControlButton = () => {
 	const status = useGlobalStateContext((v) => v.appGlobalState.processState.status)
+	const setupModalRef = useRerenderingOnceRef<ModalInterface>()
 
 	const handleSetup = () => {
 		setupModalRef.current?.open()
@@ -22,33 +20,39 @@ export const ControlButton = ({ setupModalRef }: Props) => {
 
 	const handleSave = () => {}
 
+	let controlButton;
+
 	switch (status) {
 		case ProcessStatus.WAITING_SETUP:
-			return (
+			controlButton = (
 				<Button variant='orange' onClick={handleSetup}>
 					SETUP
 				</Button>
 			)
+			break
 		case ProcessStatus.FETCHING:
-			return (
+			controlButton = (
 				<Button variant='red' onClick={handleStop}>
 					STOP
 				</Button>
 			)
+			break
 		case ProcessStatus.PAUSED:
-			return (
+			controlButton = (
 				<Button variant='green' onClick={handleResume}>
 					Resume
 				</Button>
 			)
+			break
 		case ProcessStatus.WAITING_CONFIRMATION_TO_SAVE:
-			return (
+			controlButton = (
 				<Button variant='green' onClick={handleSave}>
 					Save
 				</Button>
 			)
+			break
 		case ProcessStatus.IN_COOLDOWN:
-			return (
+			controlButton = (
 				<Button variant='blocked'>
 					<div className='flex flex-row px-6 items-center'>
 						<p className='font-medium text-base text-left leading-5'>
@@ -58,7 +62,15 @@ export const ControlButton = ({ setupModalRef }: Props) => {
 					</div>
 				</Button>
 			)
+			break
 		default:
 			throw new Error('Invalid process state')
 	}
+
+	return (
+		<>
+			<SetupModal ref={setupModalRef} />
+			{controlButton}
+		</>
+	)
 }

a07d4ce010f1570e486cfb7fd52dad66c5613f8c
deyvidfernandes <deyvid.guedes.fer@gmail.com>
2024-03-11 14:20:19 -0300

    feat: add 'wide' prop to ModalWithMessage component

--- src/components/basic/modal/ModalWithMessage.tsx
+++ src/components/basic/modal/ModalWithMessage.tsx
@@ -6,16 +6,19 @@ type Props = {
 	content: ReactNode
 	messageContent: ReactNode
 	isInMessage: boolean
+	wide?: boolean
 } & Omit<ModalProps, 'children'>
 
 export const ModalWithMessage = forwardRef<ModalInterface, Props>(
 	function ModalWithMessage(props: Props, fwdRef) {
-		const { content, messageContent, isInMessage } = props
+		const { content, messageContent, isInMessage, wide } = props
 		const ref = useModalInterfaceRef(fwdRef)
 
 		return (
-			<Modal {...props} ref={ref} unstyledContent>
-				<div className={isInMessage ? 'hidden' : 'py-4 px-6 w-full h-full'}>{content}</div>
+			<Modal {...props} ref={ref} unstyledContent wide={wide}>
+				<div className={isInMessage ? 'hidden' : 'py-4 px-6 w-full h-full'}>
+					{content}
+				</div>
 				{isInMessage && <div className={'py-4 px-6 w-full h-full'}>{messageContent}</div>}
 			</Modal>
 		)

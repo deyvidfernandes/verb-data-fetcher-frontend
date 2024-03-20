c031f3ab57544d887c5c109da4baa6014b38bf5b
deyvidfernandes <deyvid.guedes.fer@gmail.com>
2024-03-20 16:06:41 -0300

    feat: add id prop for Section component

--- src/components/basic/Section.tsx
+++ src/components/basic/Section.tsx
@@ -3,12 +3,13 @@ import { ReactNode } from 'react'
 interface Props {
 	title: string
 	children: ReactNode
+	id?: string
 }
 
-export const Section = ({ title, children }: Props) => {
+export const Section = ({ title, children, id }: Props) => {
 	return (
 		<section className='flex flex-col gap-4 w-full items-center'>
-			<h2 className='text-3xl font-semibold capitalize'>{title}</h2>
+			<h2 className='text-3xl font-semibold capitalize' id={id}>{title}</h2>
 			{children}
 		</section>
 	)

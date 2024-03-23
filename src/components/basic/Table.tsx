26f6839bcc81351ef4d1c2d9bba10ceb03f9b2c6
deyvidfernandes <deyvid.guedes.fer@gmail.com>
2024-03-23 10:21:03 -0300

    feat: add Table component

---
+++ src/components/basic/Table.tsx
@@ -0,0 +1,46 @@
+import { ListComponent, ListComponentArray } from '@/util/types'
+
+interface Props {
+	headers: ListComponent[]
+	data: ListComponentArray[]
+}
+
+export const Table = ({ headers, data }: Props) => {
+
+	const tableBodyNodes = data.map((row) => {
+		
+		if (row.components.length > headers.length)
+			throw new Error('Row has more cells than headers')
+
+		return (
+			<tr key={row.key}>
+				{row.components.map((cell) => (
+					<td  key={cell.key}>
+						<div className='flex items-center justify-center py-4 px-6'>
+							{cell.component}
+						</div>
+					</td>
+				))}
+			</tr>
+		)
+	})
+
+	const tableHeaderNodes = headers.map((header) => (
+		<th key={header.key}>{header.component}</th>
+	))
+
+	return (
+		<div className='w-full border-2 border-brandOrange rounded-lg overflow-hidden'>
+			<table className='w-full'>
+				<thead>
+					<tr>
+					{tableHeaderNodes}
+					</tr>
+				</thead>
+				<tbody>
+					{tableBodyNodes}
+				</tbody>
+			</table>
+		</div>
+	)
+}
---
+++ src/util/types.ts
@@ -0,0 +1,11 @@
+import { ReactNode } from "react"
+
+export type ListComponent = {
+	component: ReactNode, 
+	key: string
+}
+
+export type ListComponentArray = {
+	components: ListComponent[], 
+	key: string
+}

903bcb61925181ce972318dd28c63f6c0b15e8d5
deyvidfernandes <deyvid.guedes.fer@gmail.com>
2024-03-23 10:23:40 -0300

    feat: add red variant to SectionRowList component

--- src/components/VerbCard/SectionRowList.tsx
+++ src/components/VerbCard/SectionRowList.tsx
@@ -7,25 +7,43 @@ interface Props {
 		content: ReactNode
 		flex?: number
 	}[]
+	variant?: 'orange' | 'red'
+	roundedBottom?: boolean
 }
 
-export const SectionRowList = ({ sections }: Props) => {
+export const SectionRowList = ({ sections, variant = 'orange', roundedBottom }: Props) => {
+	const isRedVariant = variant === 'red'
+	const borderColorClassName = isRedVariant ? 'border-brandRed' : 'border-brandOrange'
+
 	return (
-		<div className='flex w-full h-56 border-brandOrange border-2 rounded-b-lg rounded-tr-lg'>
+		<div
+			className={`
+				flex w-full h-56 ${borderColorClassName} 
+				border-2 ${roundedBottom && 'rounded-b-lg'} rounded-tr-lg
+			`}
+		>
 			{sections.map(({ content, flex, title, id }, idx) => {
 				const isLastSection = sections.length - 1 === idx
 				return (
 					<div
 						key={id}
-						className={`flex flex-1 flex-col h-full ${
-							!isLastSection && 'border-r-2'
-						} border-brandOrange`}
+						className={`flex flex-1 flex-col h-full 
+							${!isLastSection && 'border-r-2'} 
+							${borderColorClassName}
+						`}
 						style={{ flex }}
 					>
-						<div className='h-10 border-b-2 px-8 py-2 border-brandOrange'>
+						<div className={`h-10 border-b-2 px-8 py-2 ${borderColorClassName}`}>
 							<p className='text-lg font-semibold'>{title}</p>
 						</div>
-						<div className='px-8 py-2 h-full overflow-y-scroll'>{content}</div>
+						<div
+							className={`
+								px-8 py-2 h-full overflow-y-scroll 
+								${isRedVariant && 'red-scrollbar'}
+							`}
+						>
+							{content}
+						</div>
 					</div>
 				)
 			})}
